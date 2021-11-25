61c75fb9beaf3715d0c296fb5c9c650722c0c861
Yaser Ibrahim <yaser.a.ibrahim@gmail.com>
2021-11-25 04:26:39 +0000

    function that calculates letters in string based on occurance

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🤩🤩🤩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(letters){
+  let count = {};
+  for (const elements of letters) {
+    if (count[elements]) {
+      count[elements] += 1;
+    } else {
+      if (elements !== " ") {
+        count[elements] = 1
+      }
+    }
+  }
+  return count;
+};
+
+console.log(countLetters("LHL"));
+console.log(countLetters("countLetters"));
